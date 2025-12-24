89d8611318d46deedfda04cc5e3adf65aae1496a
Zain Ali <99952754+lnzainn@users.noreply.github.com>
2025-12-24 21:50:53 +0530

    Update mbld_scrambles.py
    The changes in this PR as influenced by v2 export are:
    - added attempts table
    - modified column names to snake casing

--- mbld_scrambles.py
+++ mbld_scrambles.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from datetime import date
 
-scrambles = pd.read_csv('WCA_export_v2_350_20251216T004759Z.tsv/WCA_export_Scrambles.tsv', sep = '\t')
-results = pd.read_csv('WCA_export_v2_350_20251216T004759Z.tsv/WCA_export_Results.tsv', sep = '\t')
-comps = pd.read_csv('WCA_export_v2_350_20251216T004759Z.tsv/WCA_export_Competitions.tsv', sep = '\t')
-
+scrambles = pd.read_csv('WCA_export_v2_358_20251224T000002Z.tsv/WCA_export_scrambles.tsv', sep = '\t')
+results = pd.read_csv('WCA_export_v2_358_20251224T000002Z.tsv/WCA_export_results.tsv', sep = '\t')
+comps = pd.read_csv('WCA_export_v2_358_20251224T000002Z.tsv/WCA_export_competitions.tsv', sep = '\t')
+attempts = pd.read_csv('WCA_export_v2_358_20251224T000002Z.tsv/WCA_export_result_attempts.tsv', sep = '\t')
 
 comps['end_date'] = pd.to_datetime(
-    comps[['year', 'endMonth', 'endDay']].rename(
-        columns={'year': 'year', 'endMonth': 'month', 'endDay': 'day'}
+    comps[['end_year', 'end_month', 'end_day']].rename(
+        columns={'end_year': 'year', 'end_month': 'month', 'end_day': 'day'}
     )
 )
 
@@ -17,10 +17,10 @@ recent_comps = comps[comps['end_date'] >= cutoff]
 
 recent_ids = recent_comps['id']
 
-recent_results = results[results['competitionId'].isin(recent_ids)]
-recent_scrambles = scrambles[scrambles['competitionId'].isin(recent_ids)]
+recent_results = results[results['competition_id'].isin(recent_ids)]
+recent_scrambles = scrambles[scrambles['competition_id'].isin(recent_ids)]
 
-recent_results[recent_results['eventId'] == '333mbf']
+recent_results[recent_results['event_id'] == '333mbf']
 
 
 def mbf_scrambles_count(scramble):
@@ -49,19 +49,25 @@ def max_cubes_attempted(result_value: int) -> int:
     return attempted
 
 
-mbf_results = recent_results[recent_results['eventId'] == '333mbf']
-mbf_scrambles = recent_scrambles[recent_scrambles['eventId'] == '333mbf']
+mbf_results = recent_results[recent_results['event_id'] == '333mbf']
+mbf_scrambles = recent_scrambles[recent_scrambles['event_id'] == '333mbf']
 
-value_cols = ['value1', 'value2', 'value3']
 
-long_results = mbf_results.melt(
-    id_vars=['competitionId', 'personName'],
-    value_vars=value_cols,
-    var_name='attempt_col',
-    value_name='result_value'
+long_results = (
+    mbf_results
+    .merge(
+        attempts,
+        left_on='id',
+        right_on='result_id',
+        how='inner'
+    )
+    [['competition_id', 'person_name', 'attempt_number', 'value']]
+    .rename(columns={
+        'attempt_number': 'attempt_num',
+        'value': 'result_value'
+    })
 )
 
-long_results['attempt_num'] = long_results['attempt_col'].str.extract(r'(\d)').astype(int)
 
 long_results = long_results.dropna(subset=['result_value'])
 long_results = long_results[long_results['result_value'] > 0]
@@ -74,32 +80,32 @@ long_results['attempted'] = long_results['result_value'].apply(max_cubes_attempt
 attempts_per_person = (
     long_results
     .sort_values('attempted', ascending=False)
-    .groupby(['competitionId', 'personName'], as_index=False)
+    .groupby(['competition_id', 'person_name'], as_index=False)
     .first()
 )
 
 max_attempts_per_comp = (
     attempts_per_person
     .sort_values('attempted', ascending=False)
-    .groupby('competitionId')
+    .groupby('competition_id')
     .first()
     .reset_index()
 )
 
 
-mbf_scrambles = recent_scrambles[recent_scrambles['eventId'] == '333mbf'].copy()
+mbf_scrambles = recent_scrambles[recent_scrambles['event_id'] == '333mbf'].copy()
 
 scramble_counts = (
     mbf_scrambles
     .assign(scramble_count=mbf_scrambles['scramble'].apply(mbf_scrambles_count))
-    .groupby(['competitionId', 'scrambleNum', 'groupId'], as_index=False)['scramble_count']
+    .groupby(['competition_id', 'scramble_num', 'group_id'], as_index=False)['scramble_count']
     .sum()
 )
 
 comparison = scramble_counts.merge(
-    max_attempts_per_comp[['competitionId', 'attempt_num', 'attempted']],
-    left_on=['competitionId', 'scrambleNum'],
-    right_on=['competitionId', 'attempt_num'],
+    max_attempts_per_comp[['competition_id', 'attempt_num', 'attempted']],
+    left_on=['competition_id', 'scramble_num'],
+    right_on=['competition_id', 'attempt_num'],
     how='inner'
 )
 
@@ -129,7 +135,7 @@ def pick_best_group(df):
 
 final_cases = (
     comparison
-    .groupby('competitionId', as_index=False)
+    .groupby('competition_id', as_index=False)
     .apply(pick_best_group)
     .reset_index(drop=True)
 )
@@ -138,14 +144,14 @@ final_cases = (
 
 group_counts = (
     mbf_scrambles
-    .groupby('competitionId')['groupId']
+    .groupby('competition_id')['group_id']
     .nunique()
     .reset_index(name='group_count')
 )
 
 final_cases = final_cases.merge(
     group_counts,
-    on='competitionId',
+    on='competition_id',
     how='left'
 )
 
